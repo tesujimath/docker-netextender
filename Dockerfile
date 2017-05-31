2ff513a206da785da8d83e0b2caf2c333c236e74
Koubek <vanak.jakub@gmail.com>
2017-06-01 00:22:40 +0200

    Security improvements, cleanup.

--- Dockerfile
+++ Dockerfile
@@ -6,16 +6,11 @@ LABEL maintainer "Jakub Vanak"
 # Linux:
 #
 #   docker run -ti --privileged --name netextender --rm \
-#     -e PROXY_USER=proxy_user -e VPN_USER=vpn_user \
-#     -e VPN_DOMAIN=domain -e VPN_SERVER=server \
-#     -p 3128:3128 -v /lib/modules:/lib/modules netextender
+#     -e VPN_RDPIP=remote_terminal_ip \
+#     -e VPN_USER=vpn_user -e VPN_PASS=vpn_password \
+#     -e VPN_DOMAIN=domain -e VPN_SERVER=server \ 
+#     -p 53128:3128 -v /lib/modules:/lib/modules koubek/netextender
 #
-# OS/X:
-#
-#   docker run -ti --privileged --name netextender --rm \
-#     -e PROXY_USER=proxy_user -e VPN_USER=vpn_user \
-#     -e VPN_DOMAIN=domain -e VPN_SERVER=server \
-#     -p 3128:3128 netextender
 
 RUN \
   apt-get update && \
--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-# for v in 'PROXY_USER' 'VPN_USER' 'VPN_PASS' 'VPN_DOMAIN' 'VPN_SERVER' 'VPN_RDPIP'; do
 for v in 'VPN_USER' 'VPN_PASS'  'VPN_DOMAIN' 'VPN_SERVER' 'VPN_RDPIP'; do
     if test -z $(eval "echo \$$v"); then
         echo "Missing env variable $v" >&2
@@ -8,6 +7,7 @@ for v in 'VPN_USER' 'VPN_PASS'  'VPN_DOMAIN' 'VPN_SERVER' 'VPN_RDPIP'; do
     fi
 done
 
+iptables -F
 iptables -t nat -A PREROUTING -p tcp --dport 3380 -j DNAT --to-destination  ${VPN_RDPIP}:3389
 iptables -t nat -A POSTROUTING -j MASQUERADE
 
@@ -16,25 +16,9 @@ for iface in $(ip a | grep eth | grep inet | awk '{print $2}'); do
   iptables -t nat -A POSTROUTING -s "$iface" -j MASQUERADE
 done
 
-delayed_start()
-{
-    while test $(ps -ef | grep -v grep | grep -c nxMonitor) -eq 0; do
-        sleep 1
-    done
-    # squid3
-}
-# echo "Provide a password for web proxy user '$PROXY_USER':" >&2
-# htpasswd -c /etc/squid3/passwords "$PROXY_USER"
-# echo >&2
-delayed_start &
-
-# netExtender -u "$VPN_USER" -d "$VPN_DOMAIN" -p "$VPN_PASS" "$VPN_SERVER"
-/usr/bin/netextender
-
-#while [ true ]; do
-#  echo "------------ VPN Starts ------------"
-#  # /usr/bin/netextender
-#  netextender -u "$VPN_USER" -p "$VPN_PASS" -s "$VPN_SERVER"
-#  echo "------------ VPN exited ------------"
-#  sleep 10
-#done
+while [ true ]; do
+  echo "------------ VPN Starts ------------"
+  /usr/bin/netextender
+  echo "------------ VPN exited ------------"
+  sleep 10
+done
